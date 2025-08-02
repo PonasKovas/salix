cb4b434d88e5d8e823e591baeeda8c75d1b7d378
PonasKovas <mykolas.peteraitis@gmail.com>
2025-08-02 22:29:17 +0300

    switch to Curve25519

--- generate_cert.sh
+++ generate_cert.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-mkdir certs
-openssl ecparam -name prime256v1 -genkey -out certs/server.key
+mkdir -p certs
+openssl genpkey -algorithm ED25519 -out certs/server.key
 openssl req -new -x509 -key certs/server.key -out certs/server.crt -days 3650 -subj "/CN=unused"
