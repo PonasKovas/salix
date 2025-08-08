9710f7de5e4b8748723d52bbf483b6befaf7bbc7
PonasKovas <mykolas.peteraitis@gmail.com>
2025-08-08 21:42:56 +0300

    idk

--- client/src/main.rs
+++ client/src/main.rs
@@ -5,11 +5,11 @@ use protocol::{Empty, VERSION, main_client::MainClient, tonic::Request};
 async fn main() -> Result<()> {
 	let mut client = MainClient::connect("http://[::1]:50051").await?;
 
+	client.say_hello(Request::new(Empty {})).await?;
+
 	let server_version = client.version_check(Request::new(VERSION)).await?;
 
 	println!("server version={:?}", server_version);
-
-	client.say_hello(Request::new(Empty {})).await?;
 
 	Ok(())
 }
